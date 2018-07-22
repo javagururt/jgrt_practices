a3440dcd36afeeb13be219c4b1dcb48a07b1ea9f
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-07-22 19:02:35 +0300

    max of two test fix

--- src/test/java/com/javaguru/practices/practice_4/task1/MaxOfTwoTest.java
+++ src/test/java/com/javaguru/practices/practice_4/task1/MaxOfTwoTest.java
@@ -10,7 +10,7 @@ public class MaxOfTwoTest {
     public void getMaxOfTwo() {
         MaxOfTwo maxOfTwo = new MaxOfTwo();
         assertEquals(10, maxOfTwo.getMaxOfTwo(10, 5));
-        assertEquals(-1, maxOfTwo.getMaxOfTwo(-1, -5));
+        assertEquals(-1, maxOfTwo.getMaxOfTwo(-5, -1));
         assertEquals(30, maxOfTwo.getMaxOfTwo(30, 30));
     }
 }
