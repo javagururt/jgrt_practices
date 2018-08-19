efb7890268364961636baf915b7e615cfe73acc9
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-08-19 11:49:10 +0300

    fixed power calculator test

--- src/test/java/com/javaguru/practices/practice_6/task3/PowerCalculatorTest.java
+++ src/test/java/com/javaguru/practices/practice_6/task3/PowerCalculatorTest.java
@@ -10,6 +10,7 @@ public class PowerCalculatorTest {
     public void pow() {
         PowerCalculator pc = new PowerCalculator();
         assertEquals(Math.pow(2, 3), pc.pow(2, 3), 0.1);
+        assertEquals(Math.pow(2, 0), pc.pow(2, 0), 0.1);
         assertEquals(Math.pow(0, 3), pc.pow(0, 3), 0.1);
         assertEquals(Math.pow(10, 3), pc.pow(10, 3), 0.1);
     }
